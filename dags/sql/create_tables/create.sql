3784a7053e454899df1b24df89661fb5af74e7f8
Sophia Lawal <lawalsophia@gmail.com>
2025-02-15 19:17:56 +0100

    feat: add DDL statement

---
+++ dags/sql/create_tables/create.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA IF NOT EXISTS sales;
+
+CREATE TABLE IF NOT EXISTS sales.customer (
+    cust_id int4 NOT NULL,
+    cust_address varchar(50) NULL,
+    cust_age int4 NULL,
+    effective_start_date varchar(50) NULL,
+    effective_end_date varchar(50) NULL,
+    current_ind varchar(50) NULL,
+    CONSTRAINT customer_pk PRIMARY KEY (cust_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS sales.product (
+    product_id int4 NULL,
+    product_name varchar(50) NULL,
+    product_price int4 NULL,
+    effective_start_date varchar(50) NULL,
+    effective_end_date varchar(50) NULL,
+    current_ind varchar(50) NULL,
+    CONSTRAINT product_unique UNIQUE (product_id, effective_start_date, effective_end_date)
+);
+
+CREATE TABLE IF NOT EXISTS sales.sales_transactions (
+    order_id int4 NULL,
+    product_id int4 NULL,
+    cust_id int4 NULL,
+    product_quantity int4 NULL,
+    order_date varchar(50) NULL,
+    CONSTRAINT sales_transactions_customer_fk FOREIGN KEY (cust_id) REFERENCES sales.customer(cust_id)
+);
