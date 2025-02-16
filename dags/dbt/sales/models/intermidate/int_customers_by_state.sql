284fe8a11e5691f7f0de916aca6215097d712cad
Sophia Lawal <lawalsophia@gmail.com>
2025-02-16 20:56:41 +0100

    fix: update sql logic

--- dags/dbt/sales/models/intermidate/int_customers_by_state.sql
+++ dags/dbt/sales/models/intermidate/int_customers_by_state.sql
@@ -1,21 +1,22 @@
 with customer_count_per_state as (
 select
-    s.state,
+    state_code,
     count(1) as customer_count
 from
     {{ref('stg_customers')}} c
-join {{ref('states')}} s
-on
-    c.state_code = s.abbreviation
+
 group by
-    s.state), prct_customer_per_state as (
+    state_code), prct_customer_per_state as (
 
 
 select
-    state as state_name,
+    s.state as state_name,
     customer_count /(sum(customer_count) over()) as percentage_customer_per_state
 from
-    customer_count_per_state)
+    customer_count_per_state cs
+join {{ref('us_states')}} s
+on
+    cs.state_code = s.abbreviation)
 
 select state_name, round(percentage_customer_per_state* 100, 2) as percentage__customer
 from prct_customer_per_state
