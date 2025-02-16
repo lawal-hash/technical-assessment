e8965925cbfbb876e294ed926fcad265a642f346
Sophia Lawal <lawalsophia@gmail.com>
2025-02-16 03:38:38 +0100

    fix: reformat

--- dags/common/utils.py
+++ dags/common/utils.py
@@ -1,20 +1,19 @@
-
 import pandas as pd
-from airflow.decorators import task_group,task
+from airflow.decorators import task
 from sqlalchemy import create_engine
 
 
-@task_group
+@task
 def extract_data(table_name):
-    engine = create_engine(
-        "postgresql+psycopg2://postgres:postgres@localhost/postgres")
+    engine = create_engine("postgresql://postgres:postgres@postgres/postgres")
     path = f"dags/data/{table_name}.csv"
-    df = pd.read_csv(path,sep="[,;:]",index_col=False)
-    df.to_sql(table_name, con=engine, if_exists='replace',index=False, schema='sales')
-    
+    df = pd.read_csv(path, sep=",", index_col=False)
+    df.to_sql(table_name, con=engine, if_exists="replace", index=False, schema="sales")
+
+
 @task
 def read_table():
-    """ Return list of tables names to extract """
+    """Return list of tables names to extract"""
     return [
         "customers",
         "products",
